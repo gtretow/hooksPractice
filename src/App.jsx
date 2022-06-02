4417118d0d9a46699991997316bb427cc30a66a4
gtretow <guilherme.tretow@gmail.com>
2022-06-02 14:13:03 -0300

    understanding useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import {
-  useState,
-  useReducer,
-  useRef,
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-} from "react";
 import "./App.css";
 import axios from "axios";
+import * as React from "react";
 
 const Button = ({ ...props }) => {
   //Enquanto o useCallback mantem a referencia da função
   //O useMemo executa a função e mantem a referencia do retorno dela
-  useEffect(() => {
+  React.useEffect(() => {
     console.log("BUTTON foi re-renderizado");
 
     return () => {};
   }, []);
 
-  useEffect(() => {
+  React.useEffect(() => {
     console.log("BUTTON FUNÇÃO ONCLICK ACIONADA");
   }, [props.onClick]);
 
@@ -37,22 +29,54 @@ const UserList = ({ users, query }) => {
   //com o uso do useMemo, acabaos impedindo que o componente UserList seja renderizado toda vez que clicamos no botão de aumentar ou diminuir uma contagem, pois o valor esta memoizado
 
   //o useCallback não executa a função, já o useMemo, sim. Então ele pega o valor dessa função e memoiza.
-  const filtered = useMemo(() => filter(users, query), [users, query]);
+  const filtered = React.useMemo(() => filter(users, query), [users, query]);
   return filtered.map((user) => <div key={user.id}>{user.name}</div>);
 };
 
+//criando context
+const CountContext = React.createContext();
+
+function ContextDiv(props) {
+  const [context, setContext] = React.useState("teste");
+  const value = [context, setContext];
+  return <CountContext.Provider value={value} {...props} />;
+}
+
+function useCustomHook() {
+  const context = React.useContext(CountContext);
+
+  if (!context) {
+    throw new Error("useContext deve ser utilizado dentro de um Wrapper");
+  }
+
+  return context;
+}
+
+//gerando um wrapper pro context
+
+function ContextButton() {
+  const [context, setContext] = useCustomHook();
+  return (
+    <div>
+      <button onClick={() => setContext("contexto atualizado")}>
+        Atualizar context
+      </button>
+      <div>{context}</div>
+    </div>
+  );
+}
+
 function App({ initialCount = 0, click = 1 }) {
-  const inputEl = useRef(null);
-  const [users, setUsers] = useState([]);
-  const [query, setQuery] = useState("");
-  const [text, setText] = useState("teste de ref");
-  const [state, dispatch] = useReducer(countDispatcher, {
+  const inputEl = React.useRef(null);
+  const [users, setUsers] = React.useState([]);
+  const [query, setQuery] = React.useState("");
+  const [text, setText] = React.useState("teste de ref");
+  const [state, dispatch] = React.useReducer(countDispatcher, {
     count: initialCount,
   });
 
-  const getUsers = useCallback(async () => {
+  const getUsers = React.useCallback(async () => {
     const data = await axios.get("https://jsonplaceholder.typicode.com/users");
-    console.log("data", data);
     setUsers(data.data);
   }, []);
 
@@ -73,13 +97,13 @@ function App({ initialCount = 0, click = 1 }) {
     }
   }
 
-  useEffect(() => {
+  React.useEffect(() => {
     console.log("APP foi re-renderizado");
     getUsers();
     return () => {};
   }, [getUsers]);
 
-  useEffect(() => {
+  React.useEffect(() => {
     console.log("TEXT  ACIONADO");
 
     return () => {};
@@ -87,29 +111,31 @@ function App({ initialCount = 0, click = 1 }) {
 
   const increment = () => dispatch({ type: "INCREMENT", click });
   const decrement = () => dispatch({ type: "DECREMENT", click });
-
   const { count } = state;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Teste de hooks</p>
-        <p>
-          <button type="button" onClick={increment}>
-            Aumentar
-          </button>
-          <Button type="button" onClick={useCallback(decrement, [])}>
-            Diminuir
-          </Button>
-        </p>
-        <p>count is: {count}</p>
-        <input
-          ref={inputEl}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <input onChange={(e) => setQuery(e.target.value)} />
-        <UserList users={users} query={query}></UserList>
-      </header>
+      <ContextDiv>
+        <header className="App-header">
+          <p>Teste de hooks</p>
+          <p>
+            <button type="button" onClick={increment}>
+              Aumentar
+            </button>
+            <Button type="button" onClick={React.useCallback(decrement, [])}>
+              Diminuir
+            </Button>
+          </p>
+          <p>count is: {count}</p>
+          <input
+            ref={inputEl}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input onChange={(e) => setQuery(e.target.value)} />
+          <UserList users={users} query={query}></UserList>
+          <ContextButton />
+        </header>
+      </ContextDiv>
     </div>
   );
 }
